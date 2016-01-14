26cdb51f5d5c48d10ff2c44181e3f22ef053df43
Moch Rizky Prasetya Kurniadi <dd.mrpk@gmail.com>
2016-01-14 08:57:30 +0700

    Update db.php

--- system/config/db.php
+++ system/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=sia-ums',
     'username' => 'root',
-    'password' => 'mrizkypkmysql',
+    'password' => 'l200144020',
     'charset' => 'utf8',
 ];
